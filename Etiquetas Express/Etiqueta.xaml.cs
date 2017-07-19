9ced2f805bce9a0fc96c832aa1658a8712d5ad6f
Gabriel <tetradog@gmail.com>
2017-07-19 23:56:20 +0200

    subir archivos

---
+++ Etiquetas Express/Etiqueta.xaml.cs
@@ -0,0 +1,90 @@
+﻿/*
+ * Creado por SharpDevelop.
+ * Usuario: Pikachu240
+ * Fecha: 07/19/2017
+ * Hora: 20:52
+ * Licencia GNU GPL V3
+ * Para cambiar esta plantilla use Herramientas | Opciones | Codificación | Editar Encabezados Estándar
+ */
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace Etiquetas_Express
+{
+	/// <summary>
+	/// Interaction logic for Etiqueta.xaml
+	/// </summary>
+	public partial class Etiqueta : UserControl
+	{
+		public Etiqueta()
+		{
+			InitializeComponent();
+		}
+		public Etiqueta GenerarCopia()
+		{
+			RowDefinition row;
+			ColumnDefinition column;
+			Etiqueta etiqueta=new Etiqueta();
+			
+			etiqueta.gEtiqueta.Height=gEtiqueta.Height;
+			etiqueta.gEtiqueta.Width=gEtiqueta.Width;
+			
+			etiqueta.gEtiqueta.ColumnDefinitions.Clear();
+			for(int i=0;i<gEtiqueta.ColumnDefinitions.Count;i++){
+				column=new ColumnDefinition();
+				column.Width=gEtiqueta.ColumnDefinitions[i].Width;
+				etiqueta.gEtiqueta.ColumnDefinitions.Add(column);
+			}
+			
+			etiqueta.gEtiqueta.RowDefinitions.Clear();
+			for(int i=0;i<gEtiqueta.RowDefinitions.Count;i++){
+				row=new RowDefinition();
+				row.Height=gEtiqueta.RowDefinitions[i].Height;
+				etiqueta.gEtiqueta.RowDefinitions.Add(row);
+			}
+			etiqueta.Background=Background;
+			etiqueta.txtBody.Background=txtBody.Background;
+			return etiqueta;
+		}
+		public void PonerPlantilla(Etiqueta etiqueta)
+		{
+			RowDefinition row;
+			ColumnDefinition column;
+			
+			gEtiqueta.MinHeight=etiqueta.gEtiqueta.MinHeight;
+			gEtiqueta.MaxHeight=etiqueta.gEtiqueta.MaxHeight;
+			
+			gEtiqueta.MinWidth=etiqueta.gEtiqueta.MinWidth;
+			gEtiqueta.MaxWidth=etiqueta.gEtiqueta.MaxWidth;
+			
+			gEtiqueta.ColumnDefinitions.Clear();
+			for(int i=0;i<etiqueta.gEtiqueta.ColumnDefinitions.Count;i++)
+			{
+				column=new ColumnDefinition();
+				column.Width=etiqueta.gEtiqueta.ColumnDefinitions[i].Width;
+				gEtiqueta.ColumnDefinitions.Add(column);
+			}
+			
+			gEtiqueta.RowDefinitions.Clear();
+			for(int i=0;i<etiqueta.gEtiqueta.RowDefinitions.Count;i++)
+				{
+				row=new RowDefinition();
+				row.Height=etiqueta.gEtiqueta.RowDefinitions[i].Height;
+				gEtiqueta.RowDefinitions.Add(row);
+			}
+				
+			
+			Background=etiqueta.Background;
+			txtBody.Background=etiqueta.txtBody.Background;
+			
+		}
+	
+	}
+}
---
+++ Etiquetas Express/Window1.xaml.cs
@@ -0,0 +1,78 @@
+﻿/*
+ * Creado por SharpDevelop.
+ * Usuario: Pikachu240
+ * Fecha: 19/07/2017
+ * Hora: 20:31
+ * Licencia GNU GPL V3
+ * Para cambiar esta plantilla use Herramientas | Opciones | Codificación | Editar Encabezados Estándar
+ */
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace Etiquetas_Express
+{
+	/// <summary>
+	/// Interaction logic for Window1.xaml
+	/// </summary>
+	public partial class Window1 : Window
+	{
+		Etiqueta etiquetaPlantilla;
+		
+		public Window1()
+		{
+			etiquetaPlantilla=new Etiqueta();
+			InitializeComponent();
+		}
+		
+		public System.Windows.Controls.Primitives.UniformGrid Etiquetas
+		{
+			get{return this.ugEtiquetas;}
+		}
+		
+		void MenuImportar_Click(object sender, RoutedEventArgs e)
+		{
+			throw new NotImplementedException();
+		}
+		void MenuExportar_Click(object sender, RoutedEventArgs e)
+		{
+			throw new NotImplementedException();
+		}
+		void MenuSobre_Click(object sender, RoutedEventArgs e)
+		{
+			throw new NotImplementedException();
+		}
+		void MenuImprimir_Click(object sender, RoutedEventArgs e)
+		{
+			PrintDialog printDialog=new PrintDialog();
+			if(printDialog.ShowDialog().GetValueOrDefault())
+			{
+				printDialog.PrintVisual(ugEtiquetas,"Etiquetas");
+				
+			}else{
+				MessageBox.Show("No se ha cancelado la impresión","Cancelado",MessageBoxButton.OK,MessageBoxImage.Information);
+			}
+		}
+		void MenuEditarEtiqueta_Click(object sender, RoutedEventArgs e)
+		{
+			//Edita la plantilla
+			//pongo la plantilla
+			for(int i=0;i<ugEtiquetas.Children.Count;i++)
+				((Etiqueta)ugEtiquetas.Children[i]).PonerPlantilla(etiquetaPlantilla);
+		}
+		void BtnAñadir_Click(object sender, RoutedEventArgs e)
+		{
+			StringBuilder strEspacio1=new StringBuilder(),strEspacio2=new StringBuilder();
+			Etiqueta etiquetaNueva=etiquetaPlantilla.GenerarCopia();
+			//lo pongo en el medio
+			etiquetaNueva.txtBody.Text=strEspacio1.ToString()+txtLinea1.Text+"\n"+strEspacio2.ToString()+txtLinea2.Text;
+			ugEtiquetas.Children.Add(etiquetaNueva);
+		}
+	}
+}
